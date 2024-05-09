0581bc58767fb42057581eb36eef09f69699aa05
marianacreis <146851643+marianacreis@users.noreply.github.com>
2024-05-09 18:48:57 +0100

    Added footer information with links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Weather App</h1>
       </header>
+
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/marianacreis">Mariana Reis</a> and is{" "}
+        <a href="https://github.com/marianacreis/react-final-project">
+          open-sourced on GitHub
+        </a>{" "}
+        .
+      </footer>
     </div>
   );
 }
