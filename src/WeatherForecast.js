80f7836ab90b0f071436c3d36ae75e4565968f9d
marianacreis <146851643+marianacreis@users.noreply.github.com>
2024-05-24 16:25:05 +0100

    Added forecast for 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,7 +18,15 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <WeatherForecastDay data={forecast[0]} />
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 5) {
+            return (
+              <div key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
+          }
+        })}
       </div>
     );
   } else {
